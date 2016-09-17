7d4935b52f09a73c00e20511e41cd6be0a9504ed
vominhhoang308 <hoang.vominh96@gmail.com>
2016-09-18 00:06:04 +0300

    basic own module added

---
+++ modules/lsw/src/Controller/LSWController.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: MinhHoang
+ * Date: 9/17/2016
+ * Time: 8:35 PM
+ */
+class LSWController
+{
+
+}
---
+++ modules/lsw/src/LswServices/HelloGenerator.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: MinhHoang
+ * Date: 9/17/2016
+ * Time: 11:49 PM
+ */
+class HelloGenerator
+{
+
+}
